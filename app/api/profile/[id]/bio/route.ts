6aa6c4383d9401f41fb44577e23a1ea0b671246e
anshul_c0des <anshuldhariya5@gmail.com>
2025-08-02 22:46:54 +0530

    fixed put context typing

--- app/api/profile/[id]/bio/route.ts
+++ app/api/profile/[id]/bio/route.ts
@@ -5,7 +5,7 @@ import { User } from '@/models/User';
 import { getAuth } from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: any) {
   const { id } = context.params;
   const { userId } = getAuth(req);
 
