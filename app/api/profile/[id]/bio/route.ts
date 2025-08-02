87a3b66bcb78f16d9ed7cfc0335f9a202959c9be
anshul_c0des <anshuldhariya5@gmail.com>
2025-08-02 22:43:18 +0530

    error fixed put in profile bio

--- app/api/profile/[id]/bio/route.ts
+++ app/api/profile/[id]/bio/route.ts
@@ -5,17 +5,17 @@ import { User } from '@/models/User';
 import { getAuth } from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+  const { id } = context.params;
   const { userId } = getAuth(req);
 
-  if (userId !== params.id) {
+  if (userId !== id) {
     return new NextResponse('Unauthorized', { status: 401 });
   }
 
   const { bio } = await req.json();
 
   await dbConnect();
-
   await User.findOneAndUpdate({ clerkId: userId }, { bio });
 
   return NextResponse.json({ success: true });
